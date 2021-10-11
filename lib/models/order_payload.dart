8e390a9731d4489674d53e331f3b6944382638ba
Davide Melle <daneel87@gmail.com>
2021-10-11 19:18:54 +0200

    Update order_payload.dart

--- lib/models/order_payload.dart
+++ lib/models/order_payload.dart
@@ -334,6 +334,28 @@ class WooOrderPayloadShipping {
   }
 }
 
+class OrderMetaData {
+  int? id;
+  String? key;
+  dynamic value;
+
+  OrderMetaData({this.id, this.key, this.value});
+
+  OrderMetaData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    key = json['key'];
+    value = json['value'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['key'] = this.key;
+    data['value'] = this.value;
+    return data;
+  }
+}
+
 class LineItems {
   int? id;
   String? name;
